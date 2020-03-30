3a9ac9ccd5ffd8fab24afc0938411ae8b65c17dd
linyi <linyi@tsign.cn>
2020-03-30 17:28:04 +0800

    feat:增加邮件反馈文案

--- src/pages/tabBar/share/index.js
+++ src/pages/tabBar/share/index.js
@@ -12,8 +12,6 @@ function Index() {
             text: "",
             icon: "bookmark"
         }
-        // { title: "百度云账号", note: "", path: "/pages/tabBar/share/bdCloud/index", text: "", icon: "download-cloud" },
-        // { title: "迅雷账号", note: "", path: "/pages/tabBar/share/xunlei/index", text: "", icon: "download" }
     ];
     const openPage = e => {
         Taro.navigateTo({
@@ -30,11 +28,10 @@ function Index() {
 
     return (
         <View className="account">
-            <View className="title">
-                分享给你的朋友
-            </View>
-            <View className="content">
-                点击右上角选择转发分享给你的朋友
+            <View className="title">分享给你的朋友</View>
+            <View className="content">点击右上角选择转发分享给你的朋友</View>
+            <View className='footer-tip'>
+                问题或者建议请发送邮件致ytu_94@163.com，您的反馈是我们前进的动力。
             </View>
             <View className="adContainer">
                 <ad unit-id="adunit-49a1cea77858409e" />
